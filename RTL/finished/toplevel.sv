90e34b462235b5909c9b8db1845c4fad147f3c47
ibrkhalid11 <157555170+ibrkhalid11@users.noreply.github.com>
2026-02-15 09:45:53 -0700

    Fixed CLK_50m logic to be deterministic at reset

--- RTL/finished/toplevel.sv
+++ RTL/finished/toplevel.sv
@@ -8,13 +8,13 @@ module top_uart_crypto_basys3 (
     output logic uart_txd    // UART TX (A18)
 );
 
-    logic clk_50m;
-    always_ff @(posedge clk) begin
-        if (reset)
-            clk_50m <= 1'b0;
-        else
-            clk_50m <= ~clk_50m;
-    end
+logic clk_50m;
+always_ff @(posedge clk or posedge reset) begin
+    if (reset)
+        clk_50m <= 1'b0;
+    else
+        clk_50m <= ~clk_50m;
+end
 
     top_uart_crypto #(
         .BAUD_DIV(434)
